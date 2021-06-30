920c501713c19ec1fe3897d0da1c0b7f4c81b38b
Durgesh Pal <michaeldurgesh@gmail.com>
2021-06-30 15:41:15 +0530

    db

--- JNValumini/settings.py
+++ JNValumini/settings.py
@@ -156,5 +156,5 @@ AWS_STORAGE_BUCKET_NAME = 'jnv-alumni'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
