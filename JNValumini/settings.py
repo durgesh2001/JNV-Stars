97bb01ebb9c8511ce3aab12ef3f43cbe044b59cb
Durgesh Pal <michaeldurgesh@gmail.com>
2021-07-02 02:09:44 +0530

    heroku9

--- JNValumini/settings.py
+++ JNValumini/settings.py
@@ -156,5 +156,5 @@ AWS_S3_REGION_NAME ='us-east-2'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
