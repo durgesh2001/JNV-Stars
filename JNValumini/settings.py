fb71c3cac2a8dd06a714fdb2b5023d85d8ae3875
Durgesh Pal <michaeldurgesh@gmail.com>
2021-07-02 12:28:38 +0530

    heroku13

--- JNValumini/settings.py
+++ JNValumini/settings.py
@@ -157,5 +157,5 @@ AWS_S3_SIGNATURE_VERSION = 's3v4'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-#STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
